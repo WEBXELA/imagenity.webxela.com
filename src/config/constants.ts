57107992bc849a677bb74a9e6946f71cc607d206
YatharthChauhan2362 <yc232326@gmail.com>
2025-01-03 02:39:26 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,6 +5,6 @@ export const FONT_SIZES = {
 } as const;
 
 export const API_KEYS = {
-  REMOVE_BG: '6fqPHnuow2SGXPkuWD1nmUdY',
+  REMOVE_BG: 'grrEKHD8THW8L4Zo3fTDdSdK',
   GOOGLE_FONTS: 'AIzaSyBpjra-TDy7uSzCZy6F-NRw5qbVKl_FtZg'
 } as const;
